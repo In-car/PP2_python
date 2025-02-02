403bdee2bf791c68fcc4ba6f30cf78517d8d910e
Inkar <inkarfarkhatkyzy@gmail.com>
2025-02-02 23:48:32 +0500

    Finishing inheritance.py

--- lab3/inheritance.py
+++ lab3/inheritance.py
@@ -25,3 +25,76 @@ class Student(Person):
 
 x = Student("Aisha" , "Farkhatkyzy")
 x.printname()
+
+
+#adding __init__() function 
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    Person.__init__(self, fname, lname)
+
+x = Student("Aikorkem", "Zhumas")
+x.printname()
+
+
+#using super() function 
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+
+x = Student("Miras" , "Shokibay")
+x.printname()
+
+
+#adding properties 
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+    self.graduationyear = 2028
+
+x = Student("Inakr" , "Farkhatkyzy")
+print(x.graduationyear)
+
+
+#adding methods 
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
+x = Student("Inkar" ,"Farkhatkyzy", 2028)
+x.welcome()
