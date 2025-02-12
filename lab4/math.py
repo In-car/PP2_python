d65aca8e89b2518ad86905927c47d81085b95b85
Inkar <inkarfarkhatkyzy@gmail.com>
2025-02-12 22:35:57 +0500

    Finished math.py

---
+++ lab4/math.py
@@ -0,0 +1,38 @@
+#min() to find lowest number 
+#max() to find the highest value 
+x = min( 3, 6, 8 )
+y = max( 4, 5, 7 )
+print(x)
+print(y)
+
+
+#abs() is to return absolute value 
+z = abs(-99999)
+print(z)
+
+
+#pow(n,m) returns the value of n to the power of m 
+mynum = pow(5,2)
+print(mynum)
+
+
+#using math 
+import math
+
+
+#math.sqrt() returns the square root of number 
+x = math.sqrt(25)
+print(x)
+
+
+#math.ceil() returns the round of a number upwards 
+#math.floor() returns the round of a number downwards 
+x = math.ceil(9.99)
+y = math.floor(9.99)
+print(x)
+print(y)
+
+
+#math.pi is a constant , returns the value of PI
+x = math.pi
+print(x)
